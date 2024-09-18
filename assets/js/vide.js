e755484de828c240196bc7870df66940e8f88afe
EyaSomaii <eya.somai@esprit.tn>
2024-09-18 21:02:47 +0100

    update skills

--- assets/js/vide.js
+++ assets/js/vide.js
@@ -1,14 +1,18 @@
-document.getElementById('exampleModal').addEventListener('hidden.bs.modal', function () {
-    var video = document.getElementById('modalVideo');
+document.getElementById('exampleModal1').addEventListener('hidden.bs.modal', function () {
+    var video = document.getElementById('modalVideo1');
     video.pause();
     video.currentTime = 0;
 });
 document.getElementById('exampleModal2').addEventListener('hidden.bs.modal', function () {
-    var video = document.getElementById('modalVideo');
+    var video = document.getElementById('modalVideo2');
     video.pause();
     video.currentTime = 0;
 });
-document.getElementById('exampleModal').addEventListener('shown.bs.modal', function () {
-    var video = document.getElementById('modalVideo');
+document.getElementById('exampleModal3').addEventListener('shown.bs.modal', function () {
+    var video = document.getElementById('modalVideo3');
+    video.play();
+});
+document.getElementById('exampleModal4').addEventListener('shown.bs.modal', function () {
+    var video = document.getElementById('modalVideo4');
     video.play();
 });
